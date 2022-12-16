d8a128644db4bbbe59debbb11200773e04fecb02
all2 <stottkoraf@gmail.com>
2022-12-16 19:29:41 +0100

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,13 +19,25 @@ def data_preprocessing():
     df = pd.DataFrame()
     file = st.file_uploader("Choose a file")
     if file is not None:
+        df = pd.read_json(file)     
+        df = df.drop(['products','activityControls','description','details'], axis = 1)
+        len_1 = len(df)
+        df = df.dropna()
+        len_2 = len(df)
+        df['hour'] = df['time']
+        df['day_of_week'] = pd.DatetimeIndex(df['time']).day_of_week
+        df['year'] = pd.DatetimeIndex(df['time']).year
+        df['month'] = pd.DatetimeIndex(df['time']).month
+        df['year_month'] = df['time']
 
+        st.write('As a result of deleting damaged entries,', len_1-len_2, 'information about videos have been lost')
+        st.write('Converting json file into df are processing. Be patient, this process may take up to 2-3 minutes')
 
-        df = pd.read_json(file)
-        st.write(df.head(10))
-
-        
-        
+        for item in range(len(df)):
+            df['subtitles'].iloc[item] = df['subtitles'].iloc[item][0]['name']
+            df['hour'].iloc[item] = df['time'].iloc[item][11:13]
+            df['year_month'].iloc[item] = df['time'].iloc[item][:7]
+            df['time'].iloc[item] = df['time'].iloc[item][:10]
 
     
     
@@ -39,3 +51,20 @@ df = data_preprocessing()
 
 
 
+top_channels = df['subtitles'].value_counts()
+top_videos = df['title'].value_counts()
+top_ls = df['title'][df['subtitles']=='Lekko Stronniczy'].value_counts()
+
+st.write('Most watched videos of all time:')
+
+st.write(top_videos.head(10))
+
+
+st.write('Most watched channels of all time')
+
+st.write(top_channels.head(20))
+
+
+st.write('Most watched LS videos of all time')
+
+st.write(top_ls.head(20))
