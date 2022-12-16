6d1f6c304431dad97a137e04009ebee95da9fb7b
all2 <stottkoraf@gmail.com>
2022-12-16 19:18:46 +0100

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,18 @@
 import pandas as pd
 import os,json
 import streamlit as st
+
 pd.options.mode.chained_assignment = None
+st.title('LSowa analiza')
+
+"""
+# Dzieeeeeeeeeń dobry
+
+Stworzyłem tą aplikację po to, aby odpowiedzieć na pytani, których nigdy nawet nie zadaliście.
+
+Aby poddać analizie waszą historię YT musicie dysponować 
+[plikiem w formacie JSON zawierający waszą historię oglądania](https://www.youtube.com/watch?v=zlzzO1e6dws)
+"""
 
 def data_preprocessing():
     df = pd.DataFrame()
