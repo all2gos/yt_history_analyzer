439d85f50bcf47106ccfab872c06d7e56f63be45
all2 <stottkoraf@gmail.com>
2022-12-20 17:28:30 +0100

    streamlit commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ if file is not None:
     if compute:        
         fun = data_preprocessing(file)
         df = fun[0]
-        days_counter = datetime.date.today()-df['time'].iloc[0]
+        days_counter = datetime.date.today()-df['time'].iloc[len(df)-1]
         st.write('Od', df['time'].iloc[len(df)-1], 'zobaczyłxś',fun[1], 'filmów, co daje ', int(fun[1]/(days_counter.days)), 'zobaczonych filmów dziennie')
         st.write('Najczęściej oglądane kanały')
         st.write(df['subtitles'].value_counts())
